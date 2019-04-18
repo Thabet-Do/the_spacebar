b39024a61a0995e1ddb1e9f9bc6398c59d5f2c78
sabeat <sabeat2233@gmail.com>
2019-04-18 17:18:33 +0300

    add some service

---
+++ src/Entity/Users.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+ */
+class Users
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pass;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $priv;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPass(): ?string
+    {
+        return $this->pass;
+    }
+
+    public function setPass(string $pass): self
+    {
+        $this->pass = $pass;
+
+        return $this;
+    }
+
+    public function getPriv(): ?string
+    {
+        return $this->priv;
+    }
+
+    public function setPriv(string $priv): self
+    {
+        $this->priv = $priv;
+
+        return $this;
+    }
+}
